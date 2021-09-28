ea9d88dce16ff62cda712718167e1794d815cfd9
ghivary <ghivaryboy@gmail.com>
2021-09-28 13:09:36 +0700

    HelloWorld.java

---
+++ Pratikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama : Ghivary!");
+		System.out.println("Nim : 6706210057");
+		System.out.println("Kelas : 4503");
+	}
+}
